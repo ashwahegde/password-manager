f4866da00525be289031d7d52e0efaca7651a5fd
Ashwath Hegde <ashwathhegde.66@gmail.com>
2021-07-20 17:46:30 +0530

    removed commented lines

--- encr-decr/enc-all_first-time-setup.py
+++ encr-decr/enc-all_first-time-setup.py
@@ -35,42 +35,6 @@ IO_MESSAGES = {
     """,
 }
 
-def encrypt():
-    key = hashlib.sha256(
-        getpass.getpass(prompt="passphrase: ").encode('ascii')
-    ).hexdigest()
-    offset = int(getpass.getpass(prompt=IO_MESSAGES["offsetInput"])) % 64
-    if offset < 32:
-        f = Fernet(base64.urlsafe_b64encode(key[offset:offset+32].encode('ascii')))
-    else:
-        f = Fernet(base64.urlsafe_b64encode(key[offset:offset-32:-1].encode('ascii')))
-    del key
-    cipher_text = f.encrypt(getpass.getpass(prompt="enter plain_text: ").encode('ascii')).decode()
-    # del cipher_text
-    del f
-    return cipher_text
-
-#
-# def decrypt():
-#     key = hashlib.sha256(
-#         getpass.getpass(prompt="passphrase: ").encode('ascii')
-#     ).hexdigest()
-#     offset = int(getpass.getpass(prompt=IO_MESSAGES["offsetInput"])) % 64
-#     if offset < 32:
-#         f = Fernet(base64.urlsafe_b64encode(key[offset:offset+32].encode('ascii')))
-#     else:
-#         f = Fernet(base64.urlsafe_b64encode(key[offset:offset-32:-1].encode('ascii')))
-#     del key
-#     plain_text = None
-#     try:
-#         plain_text = f.decrypt(getpass.getpass(prompt="enter cipher_text: ").encode('ascii')).decode()
-#     except:
-#         pass
-#     # del plain_text
-#     del f
-#     return plain_text
-
-
 def main():
     # either encrypt or decrypt once per call
     key = hashlib.sha256(
@@ -101,20 +65,6 @@ def main():
                 with open(OUTPUT_MODE["appendFile_encr"],"a") as fh:
                     fh.writelines([f'\n{comment}: {cipher_text}\n'])
     del f
-    # # decrypt
-    # else:
-    #     plain_text = decrypt()
-    #     # failed case
-    #     if not plain_text:
-    #         print(IO_MESSAGES["decrFailed"])
-    #     else:
-    #         if OUTPUT_MODE["printDecr"]:
-    #             print(f'decrypted plain_text is: {plain_text}')
-    #         if OUTPUT_MODE["appendFile_decr"]:
-    #             # plain_text is very confidential so show a warning
-    #             print(IO_MESSAGES["deleteFileWarning"])
-    #             with open(OUTPUT_MODE["appendFile_decr"],"a") as fh:
-    #                 fh.writelines([f'\n{plain_text}\n'])
 
     # delete all objects in memory
     for obj in dir():
